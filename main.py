03b77e9d08c6ebd0b870bd83e02f4db7cbc3a14d
///k <71586207+sleshak@users.noreply.github.com>
2024-12-21 18:10:47 +1100

    Ужасный инет, поэтому заливаю тупо через сайт
    Переработал код полностью, добавил больше обработки. В целом, выжал максимум из pyzbara

--- main.py
+++ main.py
@@ -1,30 +1,66 @@
 import cv2
 from pyzbar.pyzbar import decode
 
-target_qr_code = "БЭК основной"
-
 cap = cv2.VideoCapture('assets\образец №2.mp4')
+output_video_path = 'output_video.mp4'
+#====================обработка
 
-while True:
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
+
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height), isColor=False)
+
+while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred_frame = cv2.GaussianBlur(gray_frame, (7, 7), 0)
 
-    decoded_objects = decode(blurred_frame)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    enhanced_frame = clahe.apply(gray_frame)
+    thresholded_frame = cv2.adaptiveThreshold(enhanced_frame, 255, 
+                                               cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                                               cv2.THRESH_BINARY, 11, 2)
 
-    found = False
-    for obj in decoded_objects:
-        qr_data = obj.data.decode('utf-8')
-        if qr_data == target_qr_code:
-            print("Нужный QR-код найден:", qr_data)
-            found = True
-            print("Позиция QR-кода:", obj.rect)
-
-if found == False:
-    print("Нужный QR-код не найден.")
+    out.write(thresholded_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+#========================================вывод
+
+video_cap = cv2.VideoCapture('output_video.mp4')
+
+while video_cap.isOpened():
+    ret, frame = video_cap.read()
+    
+    if not ret:
+        break
+    
+    #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    barcodes = decode(gray_frame)
+    
+    for barcode in barcodes:
+        qr_code_data = barcode.data.decode('utf-8')
+        
+        x, y, w, h = barcode.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        cv2.putText(frame, qr_code_data, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+    cv2.imshow('QR Detection', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_cap.release()
 cv2.destroyAllWindows()
